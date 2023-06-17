7e40d5e1150a0134536824b6f91ea635cba76ad1
viktor141 <31136336+viktor141@users.noreply.github.com>
2023-06-18 00:36:12 +0400

    Initial commit

---
+++ src/main/java/ru/viktor/repository/PostRepository.java
@@ -0,0 +1,47 @@
+package ru.viktor.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.viktor.exception.NotFoundException;
+import ru.viktor.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+@Repository
+public class PostRepository {
+
+  private final AtomicLong id = new AtomicLong(0);
+  private final ConcurrentHashMap<Long, Post> postMap = new ConcurrentHashMap<>();
+
+
+  public List<Post> all() {
+    return new ArrayList<>(postMap.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(postMap.get(id));
+  }
+
+  public Post save(Post post) {
+    if(post.getId() == 0){
+      post.setId(id.addAndGet(1));
+      postMap.put(post.getId(), post);
+      return post;
+    }
+
+    if(postMap.containsKey(post.getId())){
+      postMap.get(post.getId()).setContent(post.getContent());
+    }else {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  public void removeById(long id) {
+    postMap.remove(id);
+  }
+}
